a9b8ed65b81bffaf81df0a7334367e5e5e3db61b
e.alvarado <e.alvarado@meditrial.net>
2021-08-06 16:59:40 -0400

    Began to work with Glidejs, and got it to install need to figure out why the slide isnt working seperate testing is required

---
+++ js/glide.js
@@ -0,0 +1,10 @@
+
+const glideHero = new Glide('#miso-carousel', {
+  type: 'carousel',
+  // animationDuration: 2000,
+  // autoplay: 4500,
+  focusAt: '1',
+  startAt: 1,
+  perView: 1,
+});
+glideHero.mount();
