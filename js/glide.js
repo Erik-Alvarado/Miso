9e421bfb477c019207c408aac4164743b348d43b
e.alvarado <e.alvarado@meditrial.net>
2021-08-12 17:02:58 -0400

    Began to populate page with content, need to fit in bottom slider.

--- js/glide.js
+++ js/glide.js
@@ -8,3 +8,11 @@ const glideHero = new Glide('#miso-carousel', {
   perView: 1,
 });
 glideHero.mount();
+
+const glideFeature = new Glide('#miso-signature', {
+  type: 'carousel',
+  perView: 4,
+  gap: 20,
+
+})
+glideFeature.mount();
